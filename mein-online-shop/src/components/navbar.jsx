b478074d67ef922103b42a9e4f8bb27596d8f81e
slarixxa <larissaaa2020@gmail.com>
2024-04-16 12:52:13 +0200

    navbar and tailwind install

---
+++ mein-online-shop/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return <nav>Navigation</nav>;
+  }
+}
+export default Navbar;
---
+++ mein-online-shop/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Navbar from "./components/navbar";
+
+ReactDom.render(<Navbar></Navbar>, document.getElementById("root"));
